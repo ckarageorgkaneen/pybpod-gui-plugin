b9322850e3f534f96e90dc335284193491483be0
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2017-05-23 15:30:28 +0100

    Updates message factory for latest changes on pybpod-api

--- pybpodgui_plugin/com/messaging/msg_factory.py
+++ pybpodgui_plugin/com/messaging/msg_factory.py
@@ -110,10 +110,10 @@ def parse_board_msg(data):
 
 		elif isinstance(data, BpodTrial):
 			bpod_trial = data
-			states = bpod_trial.get_all_timestamps_by_state()
+			states = bpod_trial.states_occurrences
 			events = bpod_trial.get_all_timestamps_by_event()
 
-			for index, state in enumerate(bpod_trial.states, start=1):
+			for index, state in enumerate(states, start=1):
 				for state_duration in state.timestamps:
 					parsed_message.append(StateEntry(state.name, state_duration.start, state_duration.end, index))
 
