31ee6b92fdb861ce4553487bdf9ead406c2b7600
Carlos Mão de Ferro <cajomferro@gmail.com>
2017-06-09 17:30:57 +0100

    Adds pybpodapi logger configuration

--- pybpodgui_plugin/__init__.py
+++ pybpodgui_plugin/__init__.py
@@ -26,6 +26,10 @@ loggingbootstrap.create_double_logger("pybranch", conf.APP_LOG_HANDLER_CONSOLE_L
                                       conf.APP_LOG_FILENAME,
                                       conf.APP_LOG_HANDLER_FILE_LEVEL)
 
+loggingbootstrap.create_double_logger("pybpodapi", conf.APP_LOG_HANDLER_CONSOLE_LEVEL,
+                                      conf.APP_LOG_FILENAME,
+                                      conf.APP_LOG_HANDLER_FILE_LEVEL)
+
 if conf.USE_MULTIPROCESSING:
 	# https://docs.python.org/3.5/library/multiprocessing.html#multiprocessing.freeze_support
 	from multiprocessing import freeze_support  # @UnresolvedImport
