ad1aa1fdf5c35bb3d266f45aaf29b4f159b7ab9d
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2017-06-23 13:16:01 +0100

    Testing jenkins build

--- build_settings/win/bpod/hooks/hook-pyforms_generic_editor.py
+++ build_settings/win/bpod/hooks/hook-pyforms_generic_editor.py
@@ -2,5 +2,5 @@ from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
 hiddenimports = ["pyforms_generic_editor.settings", "pyforms_generic_editor.resources"] + collect_submodules('pyforms_generic_editor.models')
 
-datas = [('pyforms_generic_editor\\resources', 'pyforms_generic_editor\\resources')] + collect_data_files('pyforms_generic_editor')
+datas = collect_data_files('pyforms_generic_editor')
 
