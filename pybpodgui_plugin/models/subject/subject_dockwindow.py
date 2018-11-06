33c3d580ba4852afd4d6922f97e4e88d33c229d0
Luís Teixeira <micboucinha@gmail.com>
2018-11-06 10:29:36 +0000

    Added delete confirmation to the subjects

--- pybpodgui_plugin/models/subject/subject_dockwindow.py
+++ pybpodgui_plugin/models/subject/subject_dockwindow.py
@@ -21,6 +21,22 @@ class SubjectDockWindow(SubjectTreeNode):
 		"""
 		self._name.form.lineEdit.setFocus()
 
+	def remove(self):
+		"""
+
+        Prompts user to confirm subject removal and closes mdi windows associated with this subject.
+
+        .. seealso::
+            This method extends subject tree node :py:meth:`pybpodgui_plugin.models.subject.subject_treenode.SubjectTreeNode.remove`.
+
+        """
+		reply = self.question('Subject {0} will be deleted. Are you sure?'.format(self.name), 'Warning')
+		if reply == 'yes':
+			if hasattr(self, '_code_editor'):
+				self.mainwindow.mdi_area -= self._code_editor
+			super(SubjectDockWindow, self).remove()
+
+
 	def close(self, silent=False):
 		self.mainwindow.details.value = None
 		super(SubjectDockWindow, self).close(silent)
