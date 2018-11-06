64ae5f3b849b00552f9fab1b69eff82c23caa9bd
Luís Teixeira <micboucinha@gmail.com>
2018-11-06 10:55:42 +0000

    Added delete confirmation dialog for the user

--- pybpodgui_plugin/models/user/user_dockwindow.py
+++ pybpodgui_plugin/models/user/user_dockwindow.py
@@ -13,6 +13,21 @@ class UserDockWindow(UserTreeNode):
     def focus_name(self):
         self._namebox.form.lineEdit.setFocus()
 
+    def remove(self):
+        """
+
+        Prompts user to confirm user removal and closes mdi windows associated with this user.
+
+        .. seealso::
+            This method extends user tree node :py:meth:`pybpodgui_plugin.models.user.user_treenode.UserTreeNode.remove`.
+
+        """
+        reply = self.question('User {0} will be deleted. Are you sure?'.format(self.name), 'Warning')
+        if reply == 'yes':
+            if hasattr(self, '_code_editor'):
+                self.mainwindow.mdi_area -= self._code_editor
+            super(UserDockWindow, self).remove()
+
     def close(self):
         self.mainWindow.details.value = None
         super(UserDockWindow, self).close(silent)
